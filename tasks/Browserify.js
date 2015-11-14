7414dec75cef3166a25b0740260578c94dd8985c
lanjuere <lionel.anjuere@gmail.com>
2015-11-14 14:44:13 +0100

    [browserify] Fix js generator by using through2

--- tasks/Browserify.js
+++ tasks/Browserify.js
@@ -2,34 +2,56 @@
  * Created by lionel on 01/11/2015.
  */
 
-var transform = require('vinyl-transform');
 var browserify = require('browserify');
-
+var through2 = require('through2');
 
 function TaskBrowserify() {
 
-    this.createTask = function (options) {
+    var self = this;
 
-
+    self.createTask = function (options) {
         gulp.task('browserify', function () {
+            var bundle = null;
+            try {
+                bundle = self.createBundle(options);
+            } catch (err) {
+                console.error(err.message);
+                bundle = null;
+            }
 
-            var browserified = browserify(options.js.src);
-            var bundle = transform(function (filename) {
-                return browserified.bundle();
-            });
-
-
-            return gulp.src(options.js.src)
-                .pipe(bundle)
-                .on('error', function (err) {
-                    console.error(err.message);
-                    this.emit('end');
-                })
-                .pipe(gulp.dest(options.js.dest));
-            ;
+            return bundle;
         });
+    }
 
 
+    self.createBundle = function (options) {
+        gulp.src(options.js.src)
+            .pipe(through2.obj(function (file, enc, next) {
+                browserify(file.path)
+                    .transform('stripify')
+                    .bundle(function (err, res) {
+                        // assumes file.contents is a Buffer
+                        if (self.isBuffer(res)) {
+                            file.contents = res;
+                            next(null, file);
+                        } else {
+                            console.error(err.message);
+                        }
+                    });
+            })).on('error', function (err) {
+                console.error(err.message);
+                this.emit('end');
+            })
+            .pipe(gulp.dest(options.js.dest));
+    }
+
+    /**
+     * Verify if object is a readable stream
+     * @param obj
+     * @returns {boolean|string}
+     */
+    self.isBuffer = function (obj) {
+        return obj instanceof Buffer;
     }
 }
 
